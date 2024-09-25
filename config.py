76b5892cad302572af26c9353230218ec8b102e6
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-09-25 11:27:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+import os
+
+# Bot token
+TOKEN = os.getenv('BOT_TOKEN')  # Retrieve from environment variables for Koyeb
+
+# Owner and Sudo users
+OWNER_ID = int(os.getenv('OWNER_ID'))  # Telegram user ID of the bot owner
+SUDO_USERS = list(map(int, os.getenv('SUDO_USERS', '').split()))  # List of Sudo user IDs
+
+# Channel/Group IDs
+DATABASE_CHANNEL = int(os.getenv('DATABASE_CHANNEL'))  # Private channel/group for storing original PFPs
+PUBLIC_CHANNEL = int(os.getenv('PUBLIC_CHANNEL'))      # Public channel for posting compressed PFPs
+
+# Misc
+PREFIX = "/"  # Command prefix
+
+# Constants
+MAX_UPLOAD_SIZE = 20 * 1024 * 1024  # Max upload size limit (20MB)
